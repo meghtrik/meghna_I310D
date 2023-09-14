3c0673cf3785b01a49411e462b1ce662095788e2
Meghna Akshara Trikur <meghnaaksharatrikur@wireless-10-146-118-97.public.utexas.edu>
2023-09-14 16:18:26 -0500

    newfileadd

---
+++ volume_of_sphere.py
@@ -0,0 +1,5 @@
+def calculate_volume_of_sphere(radius):
+    pi = 3.14159
+    return 4/3 * pi*radius**3
+print(f'Volume of Sphere radius 30 is {calculate_volume_of_sphere(30)}')
+print(f'Volume of Sphere radius 40 is {calculate_volume_of_sphere(40)}')
